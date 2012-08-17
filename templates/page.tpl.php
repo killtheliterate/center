773e9ffd3ee0523370fe18f435c4dc57a9481917
John Ferris <john@pixel-whip.com>
2012-08-17 09:02:48 -0600

    Removed else case for logo.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -8,8 +8,6 @@
             <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
               <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
             </a>
-          <?php else: ?>
-            <a href="<?php print $base_path; ?>"><?php print $site_name; ?></a>
           <?php endif; ?>
           <?php if ($site_name): ?>
             <a href="<?php print $front_page; ?>" rel="home" id="site-name">
