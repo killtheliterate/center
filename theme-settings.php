31a1416d081996d98bcc1ad8af50a0834249838c
John Ferris <john@pixel-whip.com>
2013-09-29 10:58:50 +0930

    Add option to nuke default field markup. Defaults to true.

--- theme-settings.php
+++ theme-settings.php
@@ -20,5 +20,12 @@ function center_form_system_theme_settings_alter(&$form, $form_state) {
     '#default_value' => theme_get_setting('dev_mode'),
     '#description'   => t("Enable Center's development tools"),
   );
+
+  $form['nuke_fields'] = array(
+    '#type'          => 'checkbox',
+    '#title'         => t('Nuke Field Markup'),
+    '#default_value' => theme_get_setting('nuke_fields'),
+    '#description'   => t("Remove all markup from fields. WARNING: This will also remove labels."),
+  );
 }
 
