6daef30c5e23c7a06d6b803fcfaa1d4cc07eb94f
John Ferris <john@pixel-whip.com>
2013-09-29 09:43:48 +0930

    Add center_create_region_collection_classes() funtion.

--- template.php
+++ template.php
@@ -19,3 +19,28 @@ include 'inc/link.inc';
 include 'inc/menu.inc';
 include 'inc/page.inc';
 include 'inc/pager.inc';
+
+/**
+ * Creates a layout class based on the availability of regions in the given array.
+ *  This function is useful if you need to adjust your layout CSS based on the
+ *  presence or absence of certain regions.  Works well with pages, Panels
+ *  and Display Suite layouts.
+ *
+ * @param array $regions
+ *   An array of region machine names to check if they are empty or not.
+ *
+ * @return string
+ *  A string in the form of l--0-1... with integers representing each region in
+ *   the $regions argument.  Empty regions appear as 0. Populated regions appear
+ *   as 1.
+ */
+
+function center_create_region_collection_classes($regions) {
+  $class = 'l-';
+  // Construct a class based on if regions are empty or not.
+  foreach ($regions as $key => $region) {
+    $class .= empty($region) ? '-0' : '-1';
+  }
+  return $class;
+}
+
